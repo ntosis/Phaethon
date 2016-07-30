7d6206f803c8fc35780bba64319ba520bc55400d
Tosis Nikolaos <tosis.nikolaos@gmail.com>
2016-07-30 18:40:28 +0200

    Added Graphics for heating and cooling function.

---
+++ atmega328_Core/OLED_graphics/OLED_graphics.h
@@ -0,0 +1,21 @@
+/*
+ * OLED_graphics.h
+ *
+ *  Created on: Jul 17, 2016
+ *      Author: unix
+ */
+
+#ifndef OLED_GRAPHICS_H_
+#define OLED_GRAPHICS_H_
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#define flame_width 36
+#define flame_height 42
+#define snowflake_width 32
+#define snowflake_height 36
+
+const unsigned char flame_bits[];
+const unsigned char nifada_xioniou_bits[];
+
+#endif /* OLED_GRAPHICS_H_ */
